23c4d229fdd23a710a541776ba3fbea345e36636
muratmendil <34058160+muratmendil@users.noreply.github.com>
2018-10-15 21:09:59 +0200

    last modification

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -2,6 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MatProgressSpinnerModule } from '@angular/material';
 import { MatRadioModule } from '@angular/material/radio';
+import { MatGridListModule } from '@angular/material/grid-list';
+import { MatCardModule } from '@angular/material/card';
+import { MatTooltipModule } from '@angular/material/tooltip';
+
 
 
 @NgModule({
@@ -9,8 +13,11 @@ import { MatRadioModule } from '@angular/material/radio';
     CommonModule,
     MatProgressSpinnerModule,
     MatRadioModule,
+    MatGridListModule,
+    MatCardModule,
+    MatTooltipModule,
   ],
-  exports: [MatProgressSpinnerModule, MatRadioModule
+  exports: [MatProgressSpinnerModule, MatRadioModule, MatCardModule, MatGridListModule, MatTooltipModule
   ],
   declarations: []
 })
